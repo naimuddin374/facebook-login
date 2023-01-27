cbef09c498bfc5aa770f95eb413ec34bf1a51b56
Naim uddin <naimuddin374@gmail.com>
2023-01-27 18:56:20 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Router } from '@reach/router';
+import FacebookLogin from 'react-facebook-login';
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome Dashboard</h1>
+    </div>
+  )
+}
+const Login = () => {
+  const componentClicked = (e) => console.log('clicked', e)
+  const responseFacebook = (e) => console.log('callback', e)
+
+  return (
+    <div>
+      <h1>Welcome Login</h1>
+      <FacebookLogin
+        appId="583105393230615"
+        autoLoad={false}
+        fields="name,email,picture"
+        onClick={componentClicked}
+        callback={responseFacebook} />
+    </div>
+  )
+}
+
+const Home = () => <h1>Welcome to home page</h1>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Login path='/login' />
+      <Dashboard path='/dashboard' />
+      <Home path='/' />
+    </Router>
   );
 }
 
